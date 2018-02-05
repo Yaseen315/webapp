8f20b5e050f0e4cc715e719b9887d3061e10b9b5
Yaseen Quraishi <yaseen.quraishi@digital.hmrc.gov.uk>
2018-02-05 14:57:19 +0000

    margins

--- app.js
+++ app.js
@@ -8,7 +8,76 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
-  res.render('explore');
+
+  var recipe = [
+    {
+      image:"turkishpizza.jpg",
+      name: "Turkish Pizza",
+      prepTime: "30",
+      cuisine: "Turkish"
+    },
+    {
+      name: "Inari",
+      prepTime: "34",
+      cuisine: "Japanese"
+    },
+    {
+      name: "Lasagne",
+      prepTime: "60",
+      cuisine: "Italian"
+    },
+    {
+      name: "Pasta",
+      prepTime: "30mins",
+      cuisine: "Italian"
+    },
+    {
+      name: "Pasta",
+      prepTime: "30mins",
+      cuisine: "Italian"
+    },
+
+  ]
+
+  res.render('explore', {
+    recipe: recipe
+  });
+});
+
+app.get('/', function(req, res){
+
+  var recipe2 = [
+    {
+      name: "Turkish Pizza",
+      prepTime: "30",
+      cuisine: "Turkish"
+    },
+    {
+      name: "Inari",
+      prepTime: "34",
+      cuisine: "Japanese"
+    },
+    {
+      name: "Lasagne",
+      prepTime: "60",
+      cuisine: "Italian"
+    },
+    {
+      name: "Pasta",
+      prepTime: "30mins",
+      cuisine: "Italian"
+    },
+    {
+      name: "Pasta",
+      prepTime: "30mins",
+      cuisine: "Italian"
+    },
+
+  ]
+
+  res.render('categories', {
+    recipe2: recipe2
+  });
 });
 
 app.get("/add", function(req, res){
@@ -19,9 +88,9 @@ app.get("/detail", function(req, res){
   res.render('detail');
 });
 
-app.get("/categories", function(req, res){
-  res.render('categories');
-});
+// app.get("/categories", function(req, res){
+//   res.render('categories');
+// });
 
 app.post("/add/submit", function(req, res){
   res.send("yay")
@@ -31,3 +100,4 @@ app.post("/add/submit", function(req, res){
 app.listen(4000, function(){
   console.log("Server is running");
 });
+
---
+++ test.py
@@ -0,0 +1,10 @@
+def print_yes():
+    print('yes')
+
+def main():
+    print_yes()
+
+
+if __name__ == '__main__':
+    main()
+
