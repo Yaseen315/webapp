951cd5893182937cb1fc031f25ab39f09ac016db
Yaseen Quraishi <yaseen.quraishi@digital.hmrc.gov.uk>
2018-02-15 10:41:02 +0000

    syntax

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/add/submit", recipeController.postNew)
 app.post('/detail/:id/delete', recipeController.delete)
 
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000);
   console.log("Server is running");
 });
 
